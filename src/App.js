6cb8a368a9ff9dd3d3d9985f1f783f7202ed6b11
Benjamin Banwart <benjibanwart@gmail.com>
2022-03-21 20:33:11 -0500

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 // Import data
-
+import {postings} from './postings'
 // Import components
 import './App.css';
+import Directory from './directory'
+import Gallery from './gallery'
+import Searchbar from './searchbar'
+import Sidebar from './sidebar'
 
 function App(){
   return (
     <div>
-      <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+        <Searchbar />
+        <Directory />
+        <Sidebar />
+        <Gallery postings={postings} />
       </div>
     </div>
   );
